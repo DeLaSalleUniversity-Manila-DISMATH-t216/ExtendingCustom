4bf64b04784aa0f7de6e0be3900f59e3aad3333b
JC Bautista <JC Bautista>
2016-02-22 23:18:05 -0800

    Initial commit

---
+++ src/ph/edu/dlsu/Olive/Kalamata.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.Olive;
+
+
+public class Kalamata extends Olive {
+
+    public Kalamata() {
+        super(2);
+        this.name = "Ligurian";
+        this.flavor = "Yummy";
+        this.color = Olive.GREEN;
+
+
+    }
+}
